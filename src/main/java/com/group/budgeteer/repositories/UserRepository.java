9930316529bfa6807be7d8da750612ce825b0f02
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 11:36:36 -0400

    Add AuthUserDetailsService class and class members

--- src/main/java/com/group/budgeteer/repositories/UserRepository.java
+++ src/main/java/com/group/budgeteer/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.group.budgeteer.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -15,8 +16,15 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     /**
      * Checks if a user with a given email exists.
-     * @param email The user email address to check
+     * @param email The user email address to check.
      * @return true, if a user with the specified email exists, otherwise false.
      */
     boolean existsByEmail(String email);
+
+    /**
+     * Finds a user by their email, if it exists.
+     * @param email The user email address to check.
+     * @return An optional user if found.
+     */
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/group/budgeteer/security/AuthUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.group.budgeteer.security;
+
+
+import com.group.budgeteer.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        //returns user by email address if found, otherwise throw exception
+        return new AuthUserDetails(userRepository.findByEmail(email).orElseThrow());
+    }
+}
