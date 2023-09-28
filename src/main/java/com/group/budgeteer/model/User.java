f0c4b3eecdba38f0bf973c7e9b69517d14b1d9ea
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 10:19:05 -0400

    Add docstring to User class

--- src/main/java/com/group/budgeteer/model/User.java
+++ src/main/java/com/group/budgeteer/model/User.java
@@ -1,5 +1,9 @@
 package com.group.budgeteer.model;
 
+/**
+ * The user class represents a user entity in the application.
+ * It extends the application entity class to inherit common fields and functionality.
+ */
 public class User {
     private String email;
     private String firstName;
