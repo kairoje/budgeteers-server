aa1db1d30986375aa6430c224b8f8aac9cfb017d
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 16:42:10 -0400

    Create login method in UserService

--- src/main/java/com/group/budgeteer/services/UserService.java
+++ src/main/java/com/group/budgeteer/services/UserService.java
@@ -8,9 +8,15 @@ import com.group.budgeteer.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.logging.Logger;
 //TODO add docstrings
 @Service
@@ -42,5 +48,19 @@ public class UserService extends ApplicationService{
         userRepository.save(userObject);
 
         return jwt;
+    }
+
+    public User login(User payload, HttpServletResponse response) {
+        UsernamePasswordAuthenticationToken authenticationToken = new
+                UsernamePasswordAuthenticationToken(payload.getEmail(), payload.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            AuthUserDetails authUserDetails = (AuthUserDetails) authentication.getPrincipal();
+            String jwt = jwtUtils.generateJwtToken(authUserDetails);
+            return authUserDetails.getUser();
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid email/password");
+        }
     }
 }
