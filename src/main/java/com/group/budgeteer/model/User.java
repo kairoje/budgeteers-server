98f6cd27e9d50f37aaf87a91d92fe03de161011e
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 10:52:20 -0400

    Add docstring and SpringBoot Starter Validation

--- src/main/java/com/group/budgeteer/model/User.java
+++ src/main/java/com/group/budgeteer/model/User.java
@@ -1,9 +1,13 @@
 package com.group.budgeteer.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 /**
  * The user class represents a user entity in the application.
@@ -17,8 +21,31 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @ToString
 public class User extends ApplicationEntity<User> {
+    /**
+     * The email for user
+     */
+    @Column(unique = true, updatable = false)
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
+
+    /**
+     * The first name for user
+     */
+    @Column
     private String firstName;
+
+    /**
+     * The last name for user
+     */
+    @Column
     private String lastName;
+
+    /**
+     * The password for user
+     */
+    @Column(nullable = false)
+    @JsonProperty (access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 }
