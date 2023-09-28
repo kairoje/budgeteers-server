b60a8cbcd28c0a072c752ca0ab22ce61f35f5c6b
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 15:09:37 -0400

    Add filterChain bean

--- src/main/java/com/group/budgeteer/security/SecurityConfiguration.java
+++ src/main/java/com/group/budgeteer/security/SecurityConfiguration.java
@@ -3,7 +3,11 @@ package com.group.budgeteer.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -15,4 +19,18 @@ public class SecurityConfiguration {
 
     @Bean
     public JwtRequestFilter authJwtRequestFilter(){return new JwtRequestFilter();}
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/v1/auth/signup", "/api/v1/auth/login").permitAll()
+                .antMatchers("/h2-console").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+
+        http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
