67b8670d84039e34ddae27a3a88904096d70d824
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 14:50:17 -0400

    Create SecurityConfiguration class

---
+++ src/main/java/com/group/budgeteer/security/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.group.budgeteer.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+}
