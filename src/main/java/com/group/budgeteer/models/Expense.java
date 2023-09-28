180e34f5c927352a7ffa8f25de603adaaf394f8d
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 17:16:20 -0400

    Add class methods to Expense class

--- src/main/java/com/group/budgeteer/models/Expense.java
+++ src/main/java/com/group/budgeteer/models/Expense.java
@@ -1,6 +1,22 @@
 package com.group.budgeteer.models;
 
-public class Expense {
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "expenses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Expense extends ApplicationEntity<Expense> {
     private String name;
     private String description;
     private double price;
