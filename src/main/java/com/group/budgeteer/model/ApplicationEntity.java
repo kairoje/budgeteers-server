01f83b789c231a666a7b1a62eb199c4dcc8cc10f
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 10:16:30 -0400

    Add docstring to ApplicationEntity

--- src/main/java/com/group/budgeteer/model/ApplicationEntity.java
+++ src/main/java/com/group/budgeteer/model/ApplicationEntity.java
@@ -3,6 +3,11 @@ package com.group.budgeteer.model;
 import javax.persistence.MappedSuperclass;
 import java.util.UUID;
 
+/**
+ * The application entity class serves as a base entity for all entities in the application,
+ * providing common fields in functionality such as unique identifiers and timestamps
+ * @param <T> The type of payload that can be used to update this entity.
+ */
 @MappedSuperclass
 public class ApplicationEntity <T> {
     private UUID id;
