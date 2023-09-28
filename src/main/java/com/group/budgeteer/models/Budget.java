2c7e2f02c1724031bc04c2a60047aa4029f6dc0c
jayastronomic <juliansmith94@hotmail.com>
2023-09-28 16:28:58 -0500

    Add balance and date fields to budget

--- src/main/java/com/group/budgeteer/models/Budget.java
+++ src/main/java/com/group/budgeteer/models/Budget.java
@@ -3,8 +3,14 @@ package com.group.budgeteer.models;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
 
 @Getter
 @Setter
@@ -14,4 +20,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "budgets")
 public class Budget extends ApplicationEntity<Budget>{
+
+    @Column(nullable = false)
+    @NotNull(message = "Balance can not empty")
+    @Positive(message = "Balance must be greater than 0")
+    private Double balance;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Date can not empty")
+    private LocalDate date;
 }
