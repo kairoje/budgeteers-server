16bfb85b66b9668de4e821e70e98b1058b540ffc
jayastronomic <juliansmith94@hotmail.com>
2023-09-28 18:24:45 -0500

    Add Java Doc to BudgetRepository

--- src/main/java/com/group/budgeteer/repositories/BudgetRepository.java
+++ src/main/java/com/group/budgeteer/repositories/BudgetRepository.java
@@ -6,6 +6,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * Repository interface for managing budget data in the database.
+ *
+ * This interface extends JpaRepository, which provides CRUD (Create, Read, Update, Delete) operations
+ * for the Budget entity. Spring Data JPA will automatically generate the implementation for these methods.
+ *
+ * @see Budget
+ * @author Julian Smith
+ * @date 09/28/2023
+ */
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, UUID> {
 }
