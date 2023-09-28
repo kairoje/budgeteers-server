9a9cad4e753291cb0aa41c453f120e4831a9ca49
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 15:18:07 -0400

    Add ApplicationService class

---
+++ src/main/java/com/group/budgeteer/services/ApplicationService.java
@@ -0,0 +1,11 @@
+package com.group.budgeteer.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * The ApplicationService class provides common utility methods for accessing information about the current user
+ * within the library application.
+ */
+@Service
+public class ApplicationService {
+}
