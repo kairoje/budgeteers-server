69499373f1668e1ad234970aaf8daa873e068701
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 11:40:32 -0400

    Add docstring to AuthUserDetailsService

--- src/main/java/com/group/budgeteer/security/AuthUserDetailsService.java
+++ src/main/java/com/group/budgeteer/security/AuthUserDetailsService.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * This is the Auth User Details Service class, responsible for loading user details by their email during authentication.
+ */
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
 
@@ -18,9 +21,14 @@ public class AuthUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Loads user details by their email address
+     * @param email The email address of the user to load.
+     * @return UserDetails containing user information.
+     * @throws UsernameNotFoundException If user with specified email is not found.
+     */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        //returns user by email address if found, otherwise throw exception
         return new AuthUserDetails(userRepository.findByEmail(email).orElseThrow());
     }
 }
