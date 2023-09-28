fbf8f38d9bfbd81c0a18264227a222616da6f537
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 17:29:49 -0400

    Add expenseRepository constructor to ExpenseController

--- src/main/java/com/group/budgeteer/services/ExpenseService.java
+++ src/main/java/com/group/budgeteer/services/ExpenseService.java
@@ -2,6 +2,11 @@ package com.group.budgeteer.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class ExpenseService extends ApplicationService{
+    Logger logger = Logger.getLogger(ExpenseService.class.getName());
+
+
 }
