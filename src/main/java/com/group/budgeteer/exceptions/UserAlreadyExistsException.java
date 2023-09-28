86a36b7c7f29b5d2369799f842f8d20c2ff8c639
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 15:39:47 -0400

    Annotate UserAlreadyExistsException

--- src/main/java/com/group/budgeteer/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/group/budgeteer/exceptions/UserAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.group.budgeteer.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String message){
         super(message);
