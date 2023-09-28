746aba66571d7e3e4093aa67766a760f93a06c17
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 17:21:22 -0400

    Add docstrings to Expense class

--- src/main/java/com/group/budgeteer/models/Expense.java
+++ src/main/java/com/group/budgeteer/models/Expense.java
@@ -9,6 +9,10 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+/**
+ * The Expense class represents an expense by User in the application.
+ * It extends the ApplicationEntity class to inherit common fields and functionality.
+ */
 @Entity
 @Table(name = "expenses")
 @Getter
@@ -17,7 +21,25 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @ToString
 public class Expense extends ApplicationEntity<Expense> {
+
+    /**
+     * The name of the expense.
+     */
+    @Column(nullable = false)
+    @NotBlank(message = "Expense name cannot be blank")
     private String name;
+
+    /**
+     * A description of the expense.
+     */
+    @Column
     private String description;
+
+    /**
+     * The price of the expense.
+     */
+    @Column
+    @NotNull(message = "Expense price cannot be blank")
+    @Min(0)
     private double price;
 }
