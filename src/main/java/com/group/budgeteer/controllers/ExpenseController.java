184631ecc2570ab7600aba3510941f247bcd0a1d
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 17:32:00 -0400

    Add ExpenseService constructor to ExpenseController class

--- src/main/java/com/group/budgeteer/controllers/ExpenseController.java
+++ src/main/java/com/group/budgeteer/controllers/ExpenseController.java
@@ -1,9 +1,17 @@
 package com.group.budgeteer.controllers;
 
+import com.group.budgeteer.services.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1")
 public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    @Autowired
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
 }
