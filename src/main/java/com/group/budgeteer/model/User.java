9c8f000f671fe25f40aac5a922be483ca4195cee
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-28 10:42:55 -0400

    Replace class members with Lombok

--- src/main/java/com/group/budgeteer/model/User.java
+++ src/main/java/com/group/budgeteer/model/User.java
@@ -1,8 +1,9 @@
 package com.group.budgeteer.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.util.UUID;
 
 /**
  * The user class represents a user entity in the application.
@@ -10,60 +11,14 @@ import java.util.UUID;
  */
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User extends ApplicationEntity<User> {
     private String email;
     private String firstName;
     private String lastName;
     private String password;
-
-    public User() {
-    }
-
-    public User(String email, String firstName, String lastName, String password) {
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
