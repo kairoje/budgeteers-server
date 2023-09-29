6516474f3a683ccecef7b9d1476e613f5a90ca70
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-28 21:24:57 -0700

    Get request mapping for getting all budgets added to budget controller

--- src/main/java/com/group/budgeteer/controllers/BudgetController.java
+++ src/main/java/com/group/budgeteer/controllers/BudgetController.java
@@ -2,8 +2,12 @@ package com.group.budgeteer.controllers;
 
 import com.group.budgeteer.services.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/budget")
@@ -13,4 +17,9 @@ public class BudgetController {
 
     @Autowired
     public void setBudgetService(BudgetService budgetService) { this.budgetService = budgetService; }
+
+    @GetMapping("/budgets")
+    public List<Budget> getBudgets() { return budgetService.getBudgets(); }
+
+
 }
