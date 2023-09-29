75cf44fc511b19977e875f672992da56fdd5d0e9
jayastronomic <juliansmith94@hotmail.com>
2023-09-28 21:04:44 -0500

    Create NoSuchElementException handler

--- src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,23 @@
 package com.group.budgeteer.exceptions;
 
+import com.group.budgeteer.classes.APIResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<APIResponse<String>> handleNoSuchElementExceptions(NoSuchElementException ex){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new APIResponse<>(ex.getMessage(), "error"));
+    }
 }
