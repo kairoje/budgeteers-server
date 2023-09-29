d30fc45bb41ef0ba093015bdb85263d1e75901f9
jayastronomic <juliansmith94@hotmail.com>
2023-09-28 20:35:06 -0500

    Annotate Global Exception Handler

--- src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,10 @@
 package com.group.budgeteer.exceptions;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
 public class GlobalExceptionHandler {
+
 }
