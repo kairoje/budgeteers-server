7cd0cb357bdf5ae1d322742fcbac0420ad50bdd1
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-28 19:43:33 -0700

    Setters for expense and budget repository added to budget service class

--- src/main/java/com/group/budgeteer/services/BudgetService.java
+++ src/main/java/com/group/budgeteer/services/BudgetService.java
@@ -1,5 +1,6 @@
 package com.group.budgeteer.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -12,7 +13,15 @@ public class BudgetService {
     private ExpenseRepository expenseRepository;
 
     Logger logger = Logger.getLogger(BudgetService.class.getName()); //log messages related to the class
-    
 
+    @Autowired
+    public void setBudgetRepository(BudgetRepository budgetRepository) {
+        this.budgetRepository = budgetRepository;
+    }
+
+    @Autowired
+    public void setExpenseRepository(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
 
 }
