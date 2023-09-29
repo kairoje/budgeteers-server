d36df49bb926408e02c8c2695b2b963207ac327a
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-28 19:15:09 -0700

    Logger added to budget service

--- src/main/java/com/group/budgeteer/services/BudgetService.java
+++ src/main/java/com/group/budgeteer/services/BudgetService.java
@@ -2,11 +2,17 @@ package com.group.budgeteer.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class BudgetService {
 
     private BudgetRepository budgetRepository;
 
     private ExpenseRepository expenseRepository;
+
+    Logger logger = Logger.getLogger(BudgetService.class.getName()); //log messages related to the class
+    
+
 
 }
