581db404a5fa6421face7b28bec55da95f25c8ed
jayastronomic <juliansmith94@hotmail.com>
2023-09-28 21:12:39 -0500

    Create MethedArgumentNotValidException handler

--- src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.group.budgeteer.exceptions;
 import com.group.budgeteer.classes.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.MethodNotAllowedException;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -18,6 +21,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<APIResponse<String>> handleNoSuchElementExceptions(NoSuchElementException ex){
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(new APIResponse<>(ex.getMessage(), "error"));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<APIResponse<String>> handleMethodArgumentNotValidException(MethodNotAllowedException ex){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(new APIResponse<>(ex.getMessage(), "error"));
     }
 }
