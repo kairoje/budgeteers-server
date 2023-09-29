8e8750fca7732172f39d859f8d9fcf0f2c37b970
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-28 17:38:59 -0700

    Budget controller and budget service classes added

---
+++ src/main/java/com/group/budgeteer/controllers/BudgetController.java
@@ -0,0 +1,10 @@
+package com.group.budgeteer.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/budget")
+public class BudgetController {
+
+}
