cb8210a2b4c98c34acee2df694bfd1acd5e39510
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-28 21:19:37 -0700

    Budget service reference, and set method added to budget controller

--- src/main/java/com/group/budgeteer/controllers/BudgetController.java
+++ src/main/java/com/group/budgeteer/controllers/BudgetController.java
@@ -1,5 +1,7 @@
 package com.group.budgeteer.controllers;
 
+import com.group.budgeteer.services.BudgetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/budget")
 public class BudgetController {
 
+    private BudgetService budgetService;
+
+    @Autowired
+    public void setBudgetService(BudgetService budgetService) { this.budgetService = budgetService; }
 }
