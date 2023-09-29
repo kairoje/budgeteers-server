431ceac3abd267f38a44ecb1034061dc38f8d805
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-29 08:54:10 -0700

    refactoring for get individual budget and creating a budget

--- src/main/java/com/group/budgeteer/controllers/BudgetController.java
+++ src/main/java/com/group/budgeteer/controllers/BudgetController.java
@@ -1,10 +1,14 @@
 package com.group.budgeteer.controllers;
 
+import com.group.budgeteer.classes.APIResponse;
+import com.group.budgeteer.models.Budget;
 import com.group.budgeteer.services.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/budget")
@@ -19,10 +23,14 @@ public class BudgetController {
     public List<Budget> getBudgets() { return budgetService.getBudgets(); }
 
     @GetMapping("/budgets/{budgetId}")
-    public Optional<Budget> getBudget(@PathVariable(value = "budgetId") budgetId) { return budgetService.getBudget(budgetId); }
+    public ResponseEntity<APIResponse<Budget>> getBudget(@Valid @RequestBody Budget budgetObject){
+        return ResponseEntity
+                .ok(new APIResponse<>(budgetService.getBudget(budgetObject.getId()), "success"));
+    }
 
     @PostMapping("/budgets")
-    public Budget createBudget(@RequestBody Budget budgetObject) {
-        return budgetService.createBudget(budgetObject);
+    public ResponseEntity<APIResponse<Budget>> createBudget(@Valid @RequestBody Budget budgetObject) {
+        return ResponseEntity
+                .ok(new APIResponse<>(budgetService.createBudget(budgetObject), "success"));
     }
 }
