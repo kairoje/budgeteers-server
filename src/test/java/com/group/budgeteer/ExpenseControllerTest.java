0ac54d0edb1c6a585ac730238f67db6ee1f66517
Elizabeth Yang <elizabethyang928@gmail.com>
2023-09-30 11:26:49 -0400

    Add ExpenseControllerTest class

---
+++ src/test/java/com/group/budgeteer/ExpenseControllerTest.java
@@ -0,0 +1,8 @@
+package com.group.budgeteer;
+
+import com.group.budgeteer.controllers.ExpenseController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ExpenseController.class)
+public class ExpenseControllerTest {
+}
