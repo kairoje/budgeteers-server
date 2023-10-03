0268967bc282c1eaab768e1ca248fffbbc007f37
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-03 17:52:23 -0400

    Add docstring to UserAlreadyExistsException class

--- src/main/java/com/group/budgeteer/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/group/budgeteer/exceptions/UserAlreadyExistsException.java
@@ -2,10 +2,17 @@ package com.group.budgeteer.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-//TODO add docstrings
 
+/**
+ * Custom exception class for indicating that a user already exists in the system.
+ */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
+
+    /**
+     * Constructor for UserAlreadyExistsException.
+     * @param message A custom error message describing the reason for the exception.
+     */
     public UserAlreadyExistsException(String message){
         super(message);
     }
