25fcc04733cb6d1314ec20ebdf1fa8de6e2376e9
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-03 17:46:14 -0400

    Add docstring to GlobalExceptionHandler

--- src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/group/budgeteer/exceptions/GlobalExceptionHandler.java
@@ -16,9 +16,20 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
+/**
+ * GlobalExceptionHandler is a class that handles exceptions for the entire application.
+ * It provides exception handling for various types of exceptions and maps them to appropriate HTTP responses.
+ */
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
+
+
+    /**
+     * Exception handler for UserAlreadyExistsException.
+     * @param ex The UserAlreadyExistsException that occurred.
+     * @return ResponseEntity containing APIResponse with a conflict status and an error message.
+     */
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<APIResponse<String>> handleUserAlreadyExistExceptions(UserAlreadyExistsException ex){
@@ -26,6 +37,12 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.CONFLICT)
                 .body(new APIResponse<>(ex.getMessage(), "error"));
     }
+
+    /**
+     * Exception handler for DoesNotExistException.
+     * @param ex The DoesNotExistException that occurred.
+     * @return ResponseEntity containing APIResponse with a not found status and an error message.
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(DoesNotExistException.class)
     public ResponseEntity<APIResponse<String>> handleDoesNotExistExceptions(DoesNotExistException ex){
@@ -34,6 +51,11 @@ public class GlobalExceptionHandler {
                 .body(new APIResponse<>(ex.getMessage(), "error"));
     }
 
+    /**
+     * Exception handler for NoSuchElementException.
+     * @param ex The NoSuchElementException that occurred.
+     * @return ResponseEntity containing APIResponse with a not found status and an error message.
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<APIResponse<String>> handleNoSuchElementExceptions(NoSuchElementException ex){
@@ -42,6 +64,11 @@ public class GlobalExceptionHandler {
                 .body(new APIResponse<>(ex.getMessage(), "error"));
     }
 
+    /**
+     * Exception handler for HttpMessageNotReadableException.
+     * @param ex The HttpMessageNotReadableException that occurred.
+     * @return ResponseEntity containing APIResponse with a bad request status and an error message.
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({HttpMessageNotReadableException.class})
     public ResponseEntity<APIResponse<String>> handleHttpMessageNotReadableExceptions(HttpMessageNotReadableException ex){
@@ -50,6 +77,11 @@ public class GlobalExceptionHandler {
                 .body(new APIResponse<>(ex.getMessage(), "error"));
     }
 
+    /**
+     * Exception handler for MethodArgumentNotValidException.
+     * @param ex The MethodArgumentNotValidException that occurred.
+     * @return ResponseEntity containing error messages for validation errors.
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex){
@@ -63,6 +95,11 @@ public class GlobalExceptionHandler {
        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
+    /**
+     * Exception handler for HttpRequestMethodNotSupportedException.
+     * @param ex The HttpRequestMethodNotSupportedException that occurred.
+     * @return ResponseEntity containing error information for unsupported HTTP methods.
+     */
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<Map<String, Object>> handleMethodNotAllowedExceptions(HttpRequestMethodNotSupportedException ex){
@@ -77,4 +114,3 @@ public class GlobalExceptionHandler {
                 .body(errors);
     }
 }
-//TODO add docstring
