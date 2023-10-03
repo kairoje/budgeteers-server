2830e00c25deb164cec90714e48793de14a4d8ca
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-03 17:50:45 -0400

    Add docstring to DoesNotExistException class

--- src/main/java/com/group/budgeteer/exceptions/DoesNotExistException.java
+++ src/main/java/com/group/budgeteer/exceptions/DoesNotExistException.java
@@ -5,10 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
-//TODO add docstrings
 
+/**
+ * Custom exception class for indicating that a resource does not exist.
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class DoesNotExistException extends RuntimeException {
+
+    /**
+     * Constructor for DoesNotExistException.
+     * @param type The class type of the resource that does not exist.
+     * @param id The unique ID of the resource.
+     */
     public <T> DoesNotExistException(Class<T> type, UUID id){
         super(type.getSimpleName() + " does not exist with id: " + id);
     }
