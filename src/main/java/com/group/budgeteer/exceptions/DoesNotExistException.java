3ca347405c625c120027713b50caa28a7e0c9035
jayastronomic <juliansmith94@hotmail.com>
2023-10-02 22:02:29 -0500

    Implement DoesNotExistException Class

--- src/main/java/com/group/budgeteer/exceptions/DoesNotExistException.java
+++ src/main/java/com/group/budgeteer/exceptions/DoesNotExistException.java
@@ -1,4 +1,15 @@
 package com.group.budgeteer.exceptions;
 
-public class DoesNotExistException {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
+//TODO add docstrings
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DoesNotExistException extends RuntimeException {
+    public <T> DoesNotExistException(Class<T> type, UUID id){
+        super(type.getSimpleName() + " does not exist with id: " + id);
+    }
 }
